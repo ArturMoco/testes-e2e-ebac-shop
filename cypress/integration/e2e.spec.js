0a4664a89346f946df566aac12d5fceac4b69d8d
Artur Felipe A. Portela <artur_felipe15@hotmail.com>
2022-06-10 10:23:30 -0300

    Testes e2e usando comandos customizados - correção

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -25,7 +25,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
             .contains('Aero Daily Fitness Tee')
             .click()
 
-        cy.get('.button-variable-item-L')
+        cy.get('.button-variable-item-XS')
             .click()
         cy.get('.button-variable-item-Brown')
             .click()
@@ -133,9 +133,68 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
             .should('contain', 'Obrigado. Seu pedido foi recebido.')
         cy.get('.woocommerce-order-details__title')
             .should('contain', 'Detalhes do pedido')
-
-
     });
 
+    it.only('Deve fazer um pedido na loja Ebac Shop de ponta a ponta - Usando Comandos customizado. ', () => {
+        cy.addProdutos('Abominable Hoodie', 'L', 'Red', 1)
+        cy.get('.dropdown-toggle > .mini-cart-items')
+            .should('contain', 1)
+        cy.get('.woocommerce-message')
+            .should('contain', ' “Abominable Hoodie” foi adicionado no seu carrinho.')
 
+        cy.get('#primary-menu > .menu-item-629 > a')
+            .click()
+        cy.get(':nth-child(6) > .page-numbers')
+            .click()
+
+        cy.addProdutos('Stark Fundamental Hoodie', 'L', 'Blue', 1)
+        cy.get('.dropdown-toggle > .mini-cart-items')
+            .should('contain', 2)
+        cy.get('.woocommerce-message')
+            .should('contain', ' “Stark Fundamental Hoodie” foi adicionado no seu carrinho.')
+
+        cy.get('#primary-menu > .menu-item-629 > a')
+            .click()
+
+        cy.addProdutos('Aether Gym Pant', '33', 'Brown', 1)
+        cy.get('.dropdown-toggle > .mini-cart-items')
+            .should('contain', 3)
+        cy.get('.woocommerce-message')
+            .should('contain', ' “Aether Gym Pant” foi adicionado no seu carrinho.')
+
+        cy.get('#primary-menu > .menu-item-629 > a')
+            .click()
+
+        cy.addProdutos('Aero Daily Fitness Tee', 'XS', 'Brown', 1)
+        cy.get('.dropdown-toggle > .mini-cart-items')
+            .should('contain', 4)
+        cy.get('.woocommerce-message')
+            .should('contain', ' “Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
+
+        cy.get('.woocommerce-message > .button')
+            .click()
+        cy.get('.checkout-button')
+            .click()
+        cy.get('.showlogin')
+            .click()
+
+
+        cy.get('#username')
+            .type(dadosLogin.usuario)
+        cy.get('#password')
+            .type(dadosLogin.senha, { log: false })
+        cy.get('.woocommerce-button')
+            .click()
+
+        cy.get('#terms')
+            .click()
+        cy.get('[class="btn btn-primary btn-outline alt"]')
+            .contains('Finalizar compra')
+            .click({ force: true })
+
+        cy.get('.woocommerce-notice')
+            .should('contain', 'Obrigado. Seu pedido foi recebido.')
+        cy.get('.woocommerce-order-details__title')
+            .should('contain', 'Detalhes do pedido')
+    });
 })
